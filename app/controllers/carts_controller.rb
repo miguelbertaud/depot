566d2aed4d9471324c45ceb327f449ffe1025e78
Miguel Bertaud <miguel.bertaud@gmail.com>
2018-06-15 20:29:55 +0000

    Add Ajax and actioncable to the cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -54,15 +54,19 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_index_url,
+        notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end
 
+  # ...
   private
-    # Use callbacks to share common setup or constraints between actions.
+  # ...
+
     def set_cart
       @cart = Cart.find(params[:id])
     end
@@ -75,14 +79,5 @@ class CartsController < ApplicationController
     def invalid_cart
       logger.error "Attempt to access invalid cart #{params[:id]}"
       redirect_to store_index_url, notice: 'Invalid cart'
-    end
-    
-    def destroy
-      @cart.destroy if @cart.id == session[:cart_id] 
-      session[:cart_id] = nil
-      respond_to do |format|
-        format.html { redirect_to store_index_url, notice: 'Your cart is currently empty' }
-        format.json { head :no_content } 
-      end
     end
 end
